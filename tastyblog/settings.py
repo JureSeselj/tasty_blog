93139c82a4bc8c6d2fcfbc269698cb2e6fae5e76
JureSeselj <jure.seselj@gmail.com>
2023-01-20 19:57:38 +0000

    removed secret key

--- tastyblog/settings.py
+++ tastyblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@$l^(#_vsp7skp&^njy0sqt$3%=7ura+e)^a80t6p#u$oks8$y'
+SECRET_KEY =  os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
