85cd49b22b44f668f614080a1d3bef0ddfcd0dcd
JureSeselj <jure.seselj@gmail.com>
2023-01-21 12:46:40 +0000

    Add jquery script to open / close the search box and smooth scroll effect

--- static/js/my_script.js
+++ static/js/my_script.js
@@ -29,3 +29,25 @@ $(document).ready(function () {
     }
   });
 });
+
+// Close search box panel
+$(document).ready(function () {
+    $("#search-box .fa-times").click(function () {
+      $("#search-box").removeClass("active-box")
+    });
+  });
+  
+  // Smooth scrolling to all links
+  $("a").on('click', function (e) {
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      e.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800)
+    };
+    // End if
+  });
