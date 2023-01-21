e24c1bec907b200501a0c8d3e14fb3c51b23136b
JureSeselj <jure.seselj@gmail.com>
2023-01-21 21:51:07 +0000

    add semicolons and function to open search box panel

--- static/js/my_script.js
+++ static/js/my_script.js
@@ -4,36 +4,40 @@ $("#year").text(new Date().getFullYear());
 //Parallax effect on scrool
 $(window).scroll(function () {
   parallax();
-})
+});
 
 function parallax() {
-
   var wScroll = $(window).scrollTop();
-
   $(".parallax").css("background-position",
-    "center " + (wScroll * 0.60) + "px")
+    "center " + (wScroll * 0.60) + "px");
 }
 
 //Navbar background and color changes on scrooll
-
 $(document).ready(function () {
   $(window).scroll(function () {
     if ($(document).scrollTop() > 20) {
-      $(".navbar").addClass("nav-active")
-      $(".nav-link").addClass("nav-active")
-      $(".navbar-brand img").addClass("nav-active")
+      $(".navbar").addClass("nav-active");
+      $(".nav-link").addClass("nav-active");
+      $(".navbar-brand img").addClass("nav-active");
     } else {
-      $(".navbar").removeClass("nav-active")
-      $(".nav-link").removeClass("nav-active")
-      $(".navbar-brand img").removeClass("nav-active")
+      $(".navbar").removeClass("nav-active");
+      $(".nav-link").removeClass("nav-active");
+      $(".navbar-brand img").removeClass("nav-active");
     }
+  });
+});
+
+// Open search box panel
+$(document).ready(function () {
+  $("#search-box-button").click(function () {
+    $("#search-box").addClass("active-box");
   });
 });
 
 // Close search box panel
 $(document).ready(function () {
     $("#search-box .fa-times").click(function () {
-      $("#search-box").removeClass("active-box")
+      $("#search-box").removeClass("active-box");
     });
   });
   
@@ -47,7 +51,7 @@ $(document).ready(function () {
       // Using jQuery's animate() method to add smooth page scroll
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800)
-    };
+      }, 800);
+    }
     // End if
   });
