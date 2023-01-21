04c6616eefffa407e30b522ca37a3dadd37b279c
JureSeselj <jure.seselj@gmail.com>
2023-01-21 15:23:25 +0000

    Set X_FRAME_OPTIONS

--- tastyblog/settings.py
+++ tastyblog/settings.py
@@ -27,6 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
