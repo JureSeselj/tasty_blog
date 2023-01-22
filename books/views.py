70838b8f44897bafdfedbb92677f8fddba8b243f
JureSeselj <jure.seselj@gmail.com>
2023-01-22 15:21:32 +0000

    Add @login_required to the delete_comment, profile_view, post functions ans LoginRequiredMixin to the EditComment and PostLike class views

--- books/views.py
+++ books/views.py
@@ -8,6 +8,8 @@ from django.shortcuts import (
         render, get_object_or_404, reverse, redirect, resolve_url)
 from django.views.generic import UpdateView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
@@ -61,7 +63,8 @@ class PostDetail(View):
                 'num_comments': num_comments,
             },
         )
-
+    
+    @login_required
     def post(self, request, slug, *args, **kwargs):
         """Comment on the posts"""
         queryset = Post.objects.filter(status=1)
@@ -95,7 +98,7 @@ class PostDetail(View):
         )
 
 
-class PostLike(View):
+class PostLike(LoginRequiredMixin, View):
     """Like/Unlike posts"""
     def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
@@ -129,6 +132,7 @@ def categories_view(request, cats):
         'cats': cats.title(), 'categories_posts': categories_posts})
 
 
+@login_required
 def profile_view(request):
     """Renders the profile page"""
     if request.method == 'POST':
@@ -173,6 +177,7 @@ def search(request):
         return render(request, 'search.html', context)
 
 
+@login_required
 def delete_comment(request, comment_id):
     """Delete comment"""
     comment = get_object_or_404(Comment, id=comment_id)
@@ -182,7 +187,7 @@ def delete_comment(request, comment_id):
         'post_detail', args=[comment.post.slug]))
 
 
-class EditComment(SuccessMessageMixin, UpdateView):
+class EditComment(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     """Edite comment"""
     model = Comment
     template_name = 'edit_comment.html'
